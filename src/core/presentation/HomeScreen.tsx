cc8ec9bd5e2331684409c3da52e536a129ebf3d3
PierreVilly <pierre.villy@viacesi.fr>
2023-03-30 00:32:43 +0200

    HomeScreen init

--- src/core/presentation/HomeScreen.tsx
+++ src/core/presentation/HomeScreen.tsx
@@ -1,14 +1,38 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
-import {Text, View} from 'react-native'
+import React, { useEffect, useState } from "react";
+import { Course } from "../domain/model/Course";
+import { data } from "../utils/DummyData";
 import { RootStackParamList } from "./RootStackParamList";
+import { ScrollView, StyleSheet } from "react-native";
+import CourseCard from "./components/CourseCard";
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>
 
 export function HomeScreen({navigation}: Props){
+    const [courses, setCourses] = useState<Course[]>([])
+
+    const styles = StyleSheet.create({
+        box: {
+          flex: 1,
+          padding: 10,
+          rowGap: 10,
+          columnGap: 10
+        }
+      })
+  
+    useEffect(() => {
+        const fetchData = () => {
+            const result = data
+            setCourses(result)
+        }
+  
+        fetchData()
+    }, [])
     return(
-        <View style={{justifyContent: 'center'}}>
-            <Text>Home Screen</Text>
-        </View>
+        <ScrollView style={styles.box}>
+            {courses.map((item: Course) => (
+                CourseCard(item, {navigation})
+            ))}
+        </ScrollView>
     )
 }
